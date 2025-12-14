5b945b2826b1560540ac92162b3c8bb89edacb7d
WeirdFlexButOk7 <vardanjsp1515@gmail.com>
2025-12-14 22:41:13 +0530

    nit

--- backend/src/main/java/com/expense/config/SecurityConfig.java
+++ backend/src/main/java/com/expense/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.expense.config;
 
+import com.expense.util.JwtUtil;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.expense.util.JwtUtil;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
